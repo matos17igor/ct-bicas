257e8603a6576c7667058a38254db7dbb7b2519e
matos17igor <igorm17_pvp@hotmail.com>
2026-03-23 15:50:42 -0300

    feat: adiciona criacao de usuario com bcrypt

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { router } from "./routes.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Servidor da arena CT Bicas rodando!" });
