f4b9335e5e47dc3bb50d8455c814c454aa223fd9
matos17igor <igorm17_pvp@hotmail.com>
2026-03-24 14:15:14 -0300

    feat: adiciona o dashboard como rota privada

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Dashboard } from "./pages/Dashbord";
+import type { JSX } from "react";
+
+function PrivateRoute({ children }: { children: JSX.Element }) {
+  const token = localStorage.getItem("@CTBicas:token");
+
+  return token ? children : <Navigate to="/" />;
+}
 
 export function App() {
   return (
@@ -8,7 +15,14 @@ export function App() {
       <Routes>
         <Route path="/" element={<Login />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
 
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
